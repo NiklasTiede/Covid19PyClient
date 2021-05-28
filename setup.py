fab76dfe74aeac4812d7c9a2bcc50cc1ab221181
Niklas Tiede <niklastiede2@gmail.com>
2021-05-28 14:01:08 +0200

    fix pip install

--- setup.py
+++ setup.py
@@ -5,25 +5,25 @@ import pathlib
 from setuptools import find_packages
 from setuptools import setup
 
-from covid19pyclient import __api_src__
-from covid19pyclient import __author__
-from covid19pyclient import __email__
-from covid19pyclient import __version__
-
 setup(
     name='covid19pyclient',
-    version=__version__,
+    version='1.0.0',
     url='https://github.com/NiklasTiede/covid19pyclient',
-    author=__author__,
-    author_email=__email__,
+    author='Niklas Tiede',
+    author_email='niklastiede2@gmail.com',
     license="MIT license",
     python_requires='>=3.6',
-    description=f"A Python Wrapper around the COVID-19 API {__api_src__!r}.",
+    description="A Python Wrapper around the COVID-19 API 'https://github.com/marlon360/rki-covid-api'.",
     long_description=pathlib.Path("pypi_description.md").read_text(encoding="utf-8"),
-    # install_requires=requirements,
+    install_requires=[
+        "requests>=2.21.0",
+    ] ,
+    project_urls={
+        'Documentation': '',    # TODO enter readthedocs url
+        'Source Code': 'https://github.com/NiklasTiede/covid19pyclient',
+    },
     include_package_data=True,
     keywords='covid19pyclient',
-    # packages=find_packages(include=['covid19pyclient', 'covid19pyclient.*']),
     packages=find_packages(where="covid19pyclient"),
     setup_requires='pytest-runner',
     test_suite='tests',

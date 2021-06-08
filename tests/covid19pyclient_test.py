580769845e27ea5fe4a21ad29485f36cf908a707
Niklas Tiede <niklastiede2@gmail.com>
2021-06-08 21:47:46 +0200

    ✅ update etsts

--- tests/covid19pyclient_test.py
+++ tests/covid19pyclient_test.py
@@ -21,5 +21,18 @@ def test_germany_total():
         ('weekIncidence', float)
     ]
     for key, value in values:
-        print(key, data[key])
         assert type(data[key]) is value
+
+
+def test_germany_timeseries():
+    """Test if method returns expected data from the API endpoint."""
+    time.sleep(4)
+    covid = CovidData()
+    data = covid.germany_timeseries(days_limit=5, selected_type='deaths')['data']
+    values = [
+        ('deaths', int),
+        ('date', str),
+    ]
+    for key, value in values:
+        assert type(data[-1][key]) is value
+    assert len(data) == 5
